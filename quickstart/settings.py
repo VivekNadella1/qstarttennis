f0684af7c0322c425f03e41ba20cb78d0236244d
Annapurna Bikkina <annapurnabikkina@annapurnas-imac.lan>
2023-08-21 19:32:44 -0400

    change

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3k1iulpp4ln6y)%7*jqbooe89+oky26n9o_(u*)6g09xmri4cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
