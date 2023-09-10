1fd0cbe443d96d68b5543fe0845c31b42177cefc
Annapurna Bikkina <annapurnabikkina@annapurnas-imac.lan>
2023-09-10 15:56:51 -0400

    r

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'quickstart.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
